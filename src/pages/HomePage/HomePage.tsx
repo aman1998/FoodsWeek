ad3309cea3471aba79cd1d3765278e04c597bfab
Aman1998 <1998.amangeldi@gmail.com>
2023-05-29 17:39:19 +0600

    done: add demonstration title

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,12 +4,14 @@ import { AuthForm } from "features/Auth";
 
 import HomeHeader from "./components/HomeHeader";
 import HomeHeaderContent from "./components/HomeHeaderContent";
+import HomeDemonstration from "./components/HomeDemonstration";
 
 const HomePage: FC = () => (
   <div className="home">
     <HomeHeader />
     <div className="container">
       <HomeHeaderContent />
+      <HomeDemonstration />
     </div>
     <AuthForm />
   </div>
---
+++ src/pages/HomePage/components/HomeDemonstration/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+const HomeDemonstration: FC = () => (
+  <section className="home-demonstration">
+    <h2 className="home-demonstration__title">Find your ideal meal plan for your goals</h2>
+    <h3 className="home-demonstration__subtitle">See what FoodsWeek makes possible</h3>
+  </section>
+);
+
+export default HomeDemonstration;
